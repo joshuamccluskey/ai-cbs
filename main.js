15dee922ee5f04e423602e9bafab867a18844408
Josh McCluskey <josh.mccluskey@jm-cbs.local>
2023-10-23 20:28:36 -0700

    HuggingFace Integrated; README updated; Snapshots added

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 const { MongoClient } = require('mongodb');
+const axios = require('axios');
 require('dotenv').config();
 const uri = process.env.MONGODB_CONNECTION_STRING;
-
+const hfToken = process.env.HF_TOKEN;
+const embeddingUrl = 'https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2';
 const client = new MongoClient(uri);
+
+async function genEmbed(text) {
+    try {
+        const response = await axios.post(
+            embeddingUrl,
+            {inputs: text},
+            {headers: { Authorization: `Bearer ${hfToken}`}}
+        );
+
+        if (response.status != 200) {
+            throw new Error(`Failed see status code ${response.status} : ${response.data}`);
+        }
+
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+genEmbed('Hello World! MongoDB!');
 
 async function main() {
     try {
         await client.connect();
-        await client.db("admin").command({ ping:1 });
+        await client.db('admin').command({ ping:1 });
         console.log("🍃 Pinged! Connected to MongoDB! 🍃");
 
     } finally {
@@ -16,4 +38,4 @@ async function main() {
     }
 };
 
-main().catch(console.dir);
+//main().catch(console.dir);
